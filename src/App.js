a4cd347230d39fda3f3a321833c1a0f0719efee5
Mohammadi <a.rezamohmmadi369@gmail.com>
2024-08-08 23:05:01 +0330

    init app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+"use client";
+import React, { useEffect, useState, useRef } from "react";
+import Tree from "./components/tree";
 
-function App() {
+const createTree = (data) => {
+  const nodes = {};
+  data.forEach((item) => {
+    nodes[item.uuid] = { ...item, children: [] };
+  });
+
+  const tree = [];
+  data.forEach((item) => {
+    if (item.parent_id) {
+      nodes[item.parent_id].children.push(nodes[item.uuid]);
+    } else {
+      tree.push(nodes[item.uuid]);
+    }
+  });
+
+  return tree;
+};
+
+
+export default function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("data/response_1713955778759.json")
+      .then((response) => response.json())
+      .then((jsonData) => {
+        setData(createTree(jsonData));
+      })
+      .catch((error) => console.error("Error fetching the data:", error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 w-screen h-screen overflow-scroll justify-center items-center flex">
+      <Tree data={data} />
     </div>
   );
 }
-
-export default App;
